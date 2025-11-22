347c3b29b678dbed4b59efb183c813748b543c2a
ziggyclout <idakiallan989@gmail.com>
2025-11-22 19:11:06 +0300

    update README

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-
-
-
+// 1 Function: calculateTax
+function calculateTax(amount) {
+    return amount * 0.10;
+}
+// 2 Function: convertToUpperCase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+// 3 Function: findMaximum
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+// 4 Function: isPalindrome
+function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+// 5 Function: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+const discountAmount = (discountPercentage / 100) * originalPrice;
+return originalPrice - discountAmount;
+}
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice }; 
